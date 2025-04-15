617a6d0d0f22f62b2e72e34e97e3839757da058b
dualax <158518020+dualax@users.noreply.github.com>
2025-04-15 10:45:38 +0300

    Add files via upload

---
+++ Model/ModelContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6.Model
+{
+    public class ModelContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source = Test.db");
+            optionsBuilder.UseLazyLoadingProxies();
+        }
+    }
+}
---
+++ Pages/RegistrationPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Task6.Core;
+
+namespace Task6.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для RegistrationPage.xaml
+    /// </summary>
+    public partial class RegistrationPage : Page
+    {
+        public RegistrationPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCreate_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            Util.UtilFrame?.Navigate(new LoginPage());
+        }
+    }
+}
